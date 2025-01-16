dbbaf6ede4198171814663152b0d61657570a78f
Learnedprawn <vineshparalkar@gmail.com>
2025-01-16 16:36:44 +0530

    first test

---
+++ test/DSCEngine.t.sol
@@ -0,0 +1,14 @@
+//SPDX-license-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Test} from "forge-std/Test.sol";
+
+
+contract DSCEngineTest is Test {
+    function test() public pure {
+        // DSCEngine engine = new DSCEngine();
+        // engine.depositCollateral(address(0), 0);
+        assert(true);
+    }
+}
